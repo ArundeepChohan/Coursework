9201c1805b38102fa40924a19acad4b066b37c1d
Arundeep Singh Chohan <arundeepchohan2009@hotmail.com>
2018-11-21 11:14:58 -0800

    Update Lab7.java

--- 1150/Lab7/Lab7.java
+++ 1150/Lab7/Lab7.java
@@ -1,8 +1,6 @@
 /**
 Filename: Lab7.java
 Written by<Arundeep Chohan>
-Contact: arundeepchohan2009@hotmail.com
-Written on<Oct, 22, 2013>
 Course: Cpsc 1150-001
 Instructor: Hossein Darbandi
 Compiler: Jdk 1.7
